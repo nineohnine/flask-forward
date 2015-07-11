13500433b0f3221c0b3c790a0d219377cfbb6c9f
nineohnine <rotzbrent@gmail.com>
2015-07-11 17:48:07 -0400

    fix this

--- setup.py
+++ setup.py
@@ -1,6 +1,6 @@
 from setuptools import setup, find_packages
 setup(
-    name = "Flask-Forward",
+    name = "FlaskForward",
     version = "0.0.1",
     packages = find_packages(),
     # requirements
